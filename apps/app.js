f058ef231ad1d660e608fc14e23bb0b7465c9ac0
Emily Chen <emily.c.chen@gmail.com>
2016-05-19 22:49:15 -0400

    Update JSON get request

--- apps/app.js
+++ apps/app.js
@@ -1,5 +1,5 @@
 $(function(){
-	$.getJSON("www.omdbapi.com/?s=Star%20Wars&r=json", function(data){
-		console.log(data);
-	});
+  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+    console.log(data);
+  });
 });
