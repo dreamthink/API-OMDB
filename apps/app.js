f8bc1ce5d44e1a61099cf72d1a33e21b8d885adb
Emily Chen <emily.c.chen@gmail.com>
2016-05-23 22:34:16 -0400

    Add API functionality

--- apps/app.js
+++ apps/app.js
@@ -1,5 +1,27 @@
-$(function(){
-  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    console.log(data);
-  });
+$(document).ready(function() {
+	$("#search-term").submit(function(event){
+		event.preventDefault();
+		var searchTerm = $("#query").val();
+		getRequest(searchTerm);
+	});
 });
+
+function getRequest(searchTerm) {
+	var params = {
+		s: searchTerm,
+		r: 'json'
+	};
+	url = 'http://omdbapi.com';		
+	$.getJSON(url, params, function(data) {
+		showResults(data.Search);
+	});
+}
+
+function showResults(results) {
+	var html = "";
+	$.each(results, function(index,value) {
+		html += "<p>" + value.Title + "</p>";
+		console.log(value.Title);
+	});	
+	$("#search-results").html(html);
+};
